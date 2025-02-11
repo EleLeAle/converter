be07104599ae1d860b57a15a971189dc9a367c62
EleLeAle <felenfrancesca@icloud.com>
2025-02-11 16:43:09 -0500

    Guest check is ready

--- javaScript/guest_check.js
+++ javaScript/guest_check.js
@@ -3,8 +3,6 @@ const form = document.getElementById('data_for_guest_check');
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
-    let product_name = document.getElementById('product_name').value;
-
     let food_tot =Number(document.getElementById('food_tot').value);
 
     let bev_tot = Number(document.getElementById('bev_tot').value);
@@ -17,5 +15,29 @@ form.addEventListener('submit', function(event){
 
     let result = document.getElementById('result');
 
-    result.innerHTML = calcGuestCheck()
+    console.log(food_tot, bev_tot, pr_tax, pr_tip, tip)
+
+    result.innerHTML = calcGuestCheck(food_tot, bev_tot, pr_tax, pr_tip, tip);
+
 })
+
+function calcGuestCheck(food_tot, bev_tot, pr_tax, pr_tip, tip){
+
+    if(food_tot < 0 || bev_tot < 0 || pr_tax < 0 || tip !==0 && pr_tip !== 0 ||tip < 0 || pr_tip < 0 ){
+        return `Error. Incorrect value`
+    }
+
+    let sub_tot = food_tot + bev_tot;
+    let tax = pr_tax/100 * sub_tot;
+
+    if(pr_tip > 0){
+        tip = pr_tip/100
+    }else{
+        pr_tip = tip*100/sub_tot;
+    }
+
+    let total = (sub_tot + tax + tip).toFixed(2);
+
+    return `JAVASCRIPT CAFE<br>GUEST CHECK:<br>    Food tot : $${food_tot}<br>    Bev tot : $${bev_tot}<br>    Sub-tot : $${sub_tot}<br>    Tax pct : ${pr_tax} %<br>    Tax tot : $${tax.toFixed(2)}<br>    Tip pct : ${pr_tip} %<br>    Tip tot : ${tip.toFixed(2)}<br>    TOTAL : $${total}<br>Thank you! `
+
+}
