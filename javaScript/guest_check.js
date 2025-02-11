ffff93ac9980ff6b05204ae1a80052240b0fff2d
EleLeAle <felenfrancesca@icloud.com>
2025-02-11 15:42:00 -0500

    process

--- javaScript/guest_check.js
+++ javaScript/guest_check.js
@@ -3,6 +3,19 @@ const form = document.getElementById('data_for_guest_check');
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
+    let product_name = document.getElementById('product_name').value;
 
+    let food_tot =Number(document.getElementById('food_tot').value);
 
+    let bev_tot = Number(document.getElementById('bev_tot').value);
+
+    let pr_tax = Number(document.getElementById('pr_tax').value);
+
+    let pr_tip = Number(document.getElementById('pr_tip').value);
+
+    let tip = Number(document.getElementById('tip').value);
+
+    let result = document.getElementById('result');
+
+    result.innerHTML = calcGuestCheck()
 })
